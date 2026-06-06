b3816284d6817853585d3fdd228682d5ce97b941
Marcurio222 <mzl1004@alu.ubu.es>
2026-06-06 18:50:21 +0200

    Error de importación arreglado (subí un fichero erróneo)

--- Práctica_obligatoria_3_Zamorano_Marcos/src/main/java/com/marcos/plantio360/controller/PlantioErrorController.java
+++ Práctica_obligatoria_3_Zamorano_Marcos/src/main/java/com/marcos/plantio360/controller/PlantioErrorController.java
@@ -9,7 +9,7 @@ package com.marcos.plantio360.controller;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.boot.webmvc.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
